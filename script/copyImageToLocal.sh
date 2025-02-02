47b824cbf19a46d5c70d2752576a38ac1b9864b1
Nworm <admin@nworm.icu>
2025-02-02 22:25:56 +0800

    fix: path error

--- script/copyImageToLocal.sh
+++ script/copyImageToLocal.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 log_info() {
-    echo "[info] $@"
+    echo "[info] $*"
 }
 
+
 log_error() {
-    echo "[error] $@"
+    echo "[error] $*"
 }
 
 escape_sed() {
@@ -12,11 +13,11 @@ escape_sed() {
 }
 
 get_image_blocks() {
-   echo $(grep -Eo '!\[.*?\]\(http[s]?://.*?\)' "$1")
+   grep -Eo '!\[.*?\]\(http[s]?://.*?\)' "$1"
 }
 
 get_suitable_file_ext() {
-    grep "$(file -b --mime-type $1)" /etc/mime.types | awk '{print $2}'
+    grep "$(file -b --mime-type "$1")" /etc/mime.types | awk '{print $2}'
 }
 
 gen_unique_filename() {
@@ -46,22 +47,22 @@ download_image() {
 main() {
     local filepath="$1"
     local filename=$(basename "$1")
-    local filedir=$(dirname $filepath)
+    local filedir=$(dirname "$filepath")
     local savedir=$(realpath "$filedir/image")
-    local image_blocks=($(get_image_blocks $filepath))
+    local image_blocks=($(get_image_blocks "$filepath"))
     local new_image_blocks=()
     
-    mkdir -vp $savedir
+    mkdir -vp "$savedir"
     log_info "$1: 开始处理"
 
-    if [[ -z ${image_blocks[@]} ]]; then
+    if [[ ${#image_blocks[@]} -eq 0 ]]; then
         log_info "$1: 未找到远程图片"
         return 0
     fi
 
     log_info "$1: 已找到 ${#image_blocks[@]} 张远程图片"
 
-    for block in ${image_blocks[@]}; do
+    for block in "${image_blocks[@]}"; do
         local url=$(echo "$block" | grep -oP '\(\K[^\)]+')
         local title=$(echo "$block" | grep -oP '\[\K[^\]]+')
         local save_filename=$(gen_unique_filename "$url"  "${filename:0:-3}.$title")
@@ -71,8 +72,8 @@ main() {
 
         local image_path=''
 
-        if [[ "$(ls $savedir)" =~ $save_filename ]]; then
-            image_path=$(ls $savepath*)
+        if [[ $(ls "$savedir") =~ $save_filename ]]; then
+            image_path=$(ls "$savepath"*)
             log_info "$1: 文件已存在 $image_path"
         else
             local result=($(download_image "$url" "$savepath"))
@@ -83,25 +84,29 @@ main() {
                 continue
             fi 
 
-            image_path="${result[1]}"
+            image_path="${result[*]}"
+            image_path="${image_path:2}"
+
             log_info "$1: 下载成功 $image_path"
         fi
+        
+        image_path="./$(realpath --relative-to="$filedir" "$image_path")"
+        image_path="${image_path// /\%20}"
 
-        image_path="./$(realpath --relative-to=$filedir $image_path)"
         new_image_blocks+=("![$title]($image_path)")
     done
 
     log_info "$1: 开始替换链接"
     for index in $(seq ${#image_blocks[@]}); do
-        index=$(($index - 1))
-        local old_block=${image_blocks[$index]}
-        local new_block=${new_image_blocks[$index]}
+        index=$(( index - 1 ))
+        local old_block="${image_blocks[$index]}"
+        local new_block="${new_image_blocks[$index]}"
 
-        sed -i "s#$(escape_sed $old_block)#$(escape_sed $new_block)#g" "$filepath" 
+        sed -i "s#$(escape_sed "$old_block")#$(escape_sed "$new_block")#g" "$filepath" 
         log_info "$1: $old_block -> $new_block"
     done
 
     log_info "$1: 完成"
 }
 
-main $1
+main "$1"
